7aede59f60b1a8ff2cd370c99683c1117b1aa9eb
Parveen12218349 <pk01052003@gmail.com>
2026-02-01 13:24:28 +0530

    Switch database to SQLite for Render deployment

--- TheBarrelHouse/settings.py
+++ TheBarrelHouse/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'TheBarrelHouse.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'barreldb',
-        'USER': 'postgres',
-        'PASSWORD': '2198420Pk',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
